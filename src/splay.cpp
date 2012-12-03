34d529c9e08303f6ef6231a8df6ee44426eb021c
Andres Riofrio <riofrios@gmail.com>
2012-12-02 16:40:57 -0800

    implement splay trees, loosely based on Wikipedia's example

---
+++ src/splay.cpp
@@ -0,0 +1,144 @@
+#include "splay.h"
+#include <iostream>
+
+bool splay::insert(int x) {
+  return insert(x, root);
+}
+
+bool splay::insert(int x, node* &p) {
+  if(p == NULL) {
+    p = new struct node(x);
+    return true;
+  } else if(p->value == x) {
+    return false;
+  } else {
+    bool ret;
+    if(x < p->value) ret = insert(x, p->left);
+    else ret = insert(x, p->right);
+    return ret;
+  }
+}
+
+bool splay::contains(int x) {
+  return contains(x, root, NULL, NULL);
+}
+
+bool splay::contains(int x, node* &n, node** p, node** g) {
+  static bool splay;
+  if(n == NULL) {
+    return false;
+  } else {
+    bool ret;
+    if(n->value == x) {
+      ret = true;
+      splay = true;
+    } else {
+      if(x < n->value)
+        ret = contains(x, n->left, &n, p);
+      else
+        ret = contains(x, n->right, &n, p);
+      splay = !splay;
+    }
+    if(splay || (splay && g == NULL && p != NULL)) {
+      // print();
+      do_splay(n, p, g);
+    }
+    return ret;
+  }
+}
+
+bool splay::erase(int x) {
+  return erase(x, root);
+}
+
+bool splay::erase(int x, node* &p) {
+  if(p == NULL) {
+    return false;
+  } else if(p->value == x) {
+    erase_node(p);
+    return true;
+  } else {
+    bool ret;
+    if(x < p->value) ret = erase(x, p->left);
+    else ret = erase(x, p->right);
+    return ret;
+  }
+}
+
+// p must not be NULL
+void splay::erase_node(node* &p) {
+  if(p->left == NULL && p->right == NULL) {
+    delete p;
+    p = NULL;
+  } else if(p->left != NULL && p->right != NULL) {
+    replace_with_successor(p);
+  } else {
+    node* child;
+    if(p->left != NULL) child = p->left;
+    else child = p->right;
+    delete p;
+    p = child;
+  }
+}
+
+void splay::replace_with_successor(node* &p) {
+  replace_with_successor(p, p->right);
+}
+
+void splay::replace_with_successor(node* &p, node* &s) {
+  if(s->left == NULL) {
+    p->value = s->value;
+    erase_node(s); // never has left child
+  } else {
+    replace_with_successor(p, s->left);
+  }
+}
+
+void splay::do_splay(node* x, node** p, node** g) {
+  if(p == NULL) {
+    return;
+  } else if(g == NULL) {
+    do_splay(x, *p);
+  } else {
+    do_splay(x, *p, *g);
+  }
+}
+
+void splay::do_splay(node* x, node* &p) { // zig ztep
+  if(x == p->left) {
+    right_rotation(p);
+  } else {
+    left_rotation(p);
+  }
+}
+
+void splay::do_splay(node* x, node* &p, node* &g) {
+  if(x == p->left && p == g->left) { // zig-zig step
+    right_rotation(g);
+    right_rotation(g);
+  } else if(x == p->right && p == g->right) { // zig-zig step
+    left_rotation(g);
+    left_rotation(g);
+  } else if(x == p->right && p == g->left) { // zig-zag step
+    left_rotation(p);
+    right_rotation(g);
+  } else if(x == p->left && p == g->right) { // zig-zag step
+    right_rotation(p);
+    left_rotation(g);
+  }
+}
+
+void splay::right_rotation(node* &p) {
+  node* x = p->left;
+  p->left = x->right;
+  x->right = p;
+  p = x;
+}
+
+void splay::left_rotation(node* &p) {
+  node* x = p->right;
+  p->right = x->left;
+  x->left = p;
+  p = x;
+}
+
--- src/splay.h
+++ src/splay.h
@@ -5,9 +5,22 @@
 
 class splay : public bst {
   virtual ~splay() {}
-  virtual bool insert(int x) {}
-  virtual bool contains(int x) {}
-  virtual bool erase(int x) {}
+  virtual bool insert(int x);
+  virtual bool contains(int x);
+  virtual bool erase(int x);
+
+  static bool insert(int x, node* &p);
+  static bool contains(int x, node* &n, node** p, node** g);
+  static bool erase(int x, node* &p);
+  static void erase_node(node* &p);
+  static void replace_with_successor(node* &p);
+  static void replace_with_successor(node* &p, node* &s);
+
+  static void do_splay(node* x, node** p, node** g);
+  static void do_splay(node* x, node* &p);
+  static void do_splay(node* x, node* &p, node* &g);
+  static void right_rotation(node* &p);
+  static void left_rotation(node* &p);
 };
 
 #endif
